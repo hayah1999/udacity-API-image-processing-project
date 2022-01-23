8541ab04f52a01c44214e0270f73e4c78186da4a
Hayah Hazem <88937982+hayah1999@users.noreply.github.com>
2022-01-23 14:10:50 +0200

    Update sharpSpec.ts

--- src/tests/sharpSpec.ts
+++ src/tests/sharpSpec.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import imageResize from '../routes/utilites/sharp';
 
 it('tests funcationality of sharp function for image processing ', () => {
@@ -11,34 +10,3 @@ it('tests funcationality of sharp function for image processing ', () => {
   size: 350498,
  });
 });
-||||||| f1d0ea9
-=======
-import sharp from 'sharp';
-
-it('tests funcationality of sharp function for image processing ', () => {
- const image: string = './images/fjord.jpg';
-
- function imageResize(imagePath: string): object {
-  const resize = sharp(imagePath)
-   .resize({
-    width: 400,
-    height: 400,
-    fit: sharp.fit.fill,
-   })
-   .toFile(`./images/thumbs/fjorrr_400_400.png`)
-   //https://sharp.pixelplumbing.com/api-resize
-   .then((data) => {
-    console.log(data);
-   });
-  return resize;
- }
- expect(imageResize(image)).toBeTruthy({
-  format: 'png',
-  width: 400,
-  height: 400,
-  channels: 3,
-  premultiplied: false,
-  size: 350498,
- });
-});
->>>>>>> 38eeb6eaa69053ae9b4da6e92618251a18ed3620
